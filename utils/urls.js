5dc2b18534ba9d9bdd3c24fd6bcf0c7419c3ddb5
nextazy <nextazysolutions@gmail.com>
2018-11-28 16:08:51 +0530

    changed etherscan api url for testing purpose to ropsten

--- utils/urls.js
+++ utils/urls.js
@@ -1,6 +1,6 @@
 const urls = {
     infura : "https://mainnet.infura.io/v3/d5fbbe579b3c4d108a0c9a30b74850f0",
-    etherscanGetAbi : "https://api.etherscan.io/api?module=contract&action=getabi&address="
+    etherscanGetAbi : "https://api-ropsten.etherscan.io/api?module=contract&action=getabi&address="
 };
 
 module.exports = urls;
